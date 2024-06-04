b5b6bad8ce5f3f72640d4c596ed79de2b74f5514
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-06-04 11:12:17 +0300

    Solve task 03 in homework 04

--- lesson_04/homework_04.py
+++ lesson_04/homework_04.py
@@ -38,7 +38,7 @@ print(adwentures_of_tom_sawer)
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
 print("\ntask 03")
-adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("  ", "")
+adwentures_of_tom_sawer = " ".join(adwentures_of_tom_sawer.split())
 print(adwentures_of_tom_sawer)
 
 # task 04
