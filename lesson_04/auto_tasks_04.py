fea61e0c31fb91363e25c28cd527f81291546d6f
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-06-07 17:25:42 +0300

    solve autotasks for homework 04

---
+++ lesson_04/auto_tasks_04.py
@@ -0,0 +1,33 @@
+# task 01
+# def solution(test_string):
+#     test_string = test_string.split(": ")
+#     return test_string[-1]
+#
+#
+# successful = "2023-04-27 15:30:45 - TestCase: login_successful"
+# invalid = "2023-04-27 15:35:12 - TestCase: invalid_password"
+# print(solution(successful))
+# print(solution(invalid))
+
+# task 02
+def check_file_format(file_list: list, extention: str):
+    new_list = []
+    for file in file_list:
+        if file.endswith(extention):
+            new_list.append(file)
+    return new_list
+
+
+list = ["a.txt", "b.txt", "c.log", "d.html", "e.log", ".diff"]
+ext_01 = ".txt"
+print(check_file_format(list, ext_01))
+print(check_file_format(list, ext_01) == ["a.txt", "b.txt"])
+
+ext_02 = ".log"
+print(check_file_format(list, ext_02))
+print(check_file_format(list, ext_02) == ["c.log", "e.log"])
+
+ext_03 = ".json"
+# Очікуваний список = []
+print("task 04")
+print(check_file_format(list, ext_03))
