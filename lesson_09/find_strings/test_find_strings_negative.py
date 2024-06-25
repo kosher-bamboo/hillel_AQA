cedc82a5cbef62dd832626b70cf4dc05c061dbc9
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-06-25 19:01:27 +0300

    edits in homework 09

--- lesson_09/find_strings/test_find_strings_negative.py
+++ lesson_09/find_strings/test_find_strings_negative.py
@@ -1,7 +1,27 @@
 import unittest
 
-from lesson_07.homework_07 import even_number_sum
+from lesson_09.functions import find_strings
 
 
 class FindStringsNegativeTest(unittest.TestCase):
-    def test_find_strings(self):
+    def test_find_strings_None(self):
+
+        input_value = None
+        expected_result = "'NoneType' object is not iterable"
+
+        with self.assertRaises(TypeError) as error:
+            find_strings(input_value)
+        error_message = error.exception.args[0]
+
+        self.assertEqual(error_message, expected_result)
+
+    def test_find_strings_number(self):
+
+        input_value = 24
+        expected_result = "'int' object is not iterable"
+
+        with self.assertRaises(TypeError) as error:
+            find_strings(input_value)
+        error_message = error.exception.args[0]
+
+        self.assertEqual(error_message, expected_result)
--- lesson_09/find_strings/test_find_strings_positive.py
+++ lesson_09/find_strings/test_find_strings_positive.py
@@ -1,7 +1,26 @@
 import unittest
 
-from lesson_07.homework_07 import even_number_sum
+from lesson_09.functions import find_strings
 
 
 class FindStringsPositiveTest(unittest.TestCase):
     def test_find_strings(self):
+
+        input_value = [-94.79, 'vjqbxrguip', -58]
+        expected_result = ['vjqbxrguip']
+
+        self.assertEqual(find_strings(input_value), expected_result)
+
+    def test_find_strings_numbers(self):
+
+        input_value = [-94.79, -58, 60, 43, 54, 40, 5]
+        expected_result = []
+
+        self.assertEqual(find_strings(input_value), expected_result)
+
+    def test_find_strings_empty(self):
+
+        input_value = []
+        expected_result = []
+
+        self.assertEqual(find_strings(input_value), expected_result)
