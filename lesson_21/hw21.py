9116b8ff3d248cb5cf7bb10e9cc98c6cfe914d38
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-08-13 18:10:12 +0300

    homework 21: code wrapped in functions

--- lesson_21/hw21.py
+++ lesson_21/hw21.py
@@ -5,37 +5,53 @@ logging.basicConfig(filename='hb_test.log',
                     level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-with open('hblog.txt', 'r') as file:
-    file_content = file.read()
-    file_content = file_content.split('\n')
 
-# filter log by 'Key'
-filtered_log = []
-for row in file_content:
-    if 'Key TSTFEED0300|7E3E|0400' in row:
-        filtered_log.append(row)
-        pass
+def convert_log_to_list_of_strings(file):
+    with open(file, 'r') as file:
+        file_content = file.read()
+        return file_content.split('\n')
 
-# get every time from filtered rows
-list_of_times = []
-for row in filtered_log:
-    timestamp_text = 'Timestamp '
-    start_position = row.find(timestamp_text) + len(timestamp_text)
-    # get times
-    time_string = row[start_position:start_position + 8]
-    list_of_times.append(datetime.strptime(time_string, "%H:%M:%S"))
-    pass
 
-td_31 = timedelta(seconds=31)
-td_33 = timedelta(seconds=33)
+def filter_log_by_key(log_list: list, filter_key: str):
+    # filter log by key
+    filtered_log = []
+    for row in log_list:
+        if filter_key in row:
+            filtered_log.append(row)
+    return filtered_log
 
-list_of_times_index = 0
-for time in list_of_times:
-    while list_of_times_index < len(list_of_times) - 1:
-        if td_31 < (list_of_times[list_of_times_index] - list_of_times[list_of_times_index + 1]) < td_33:
-            logging.warning(f"\n{filtered_log[list_of_times_index]}"
-                            f"\n{filtered_log[list_of_times_index + 1]}")
-        elif (list_of_times[list_of_times_index] - list_of_times[list_of_times_index + 1]) > td_33:
-            logging.error(f"\n{filtered_log[list_of_times_index]}"
-                          f"\n{filtered_log[list_of_times_index + 1]}")
-        list_of_times_index += 1
+
+def analyze_log(filtered_log: list):
+    # get every time from filtered rows
+    list_of_times = []
+    for row in filtered_log:
+        timestamp_text = 'Timestamp ' # get index for substring
+        start_position = row.find(timestamp_text) + len(timestamp_text) # get start position for time
+        time_string = row[start_position:start_position + 8] # time format 00:00:00 is 8 characters length
+        list_of_times.append(datetime.strptime(time_string, "%H:%M:%S")) # create a list with times
+
+    # timedelta objects for future comparison
+    td_31 = timedelta(seconds=31)
+    td_33 = timedelta(seconds=33)
+
+    list_of_times_index = 0
+    for _ in list_of_times:
+        while list_of_times_index < len(list_of_times) - 1:
+            # get a difference between current and next time record
+            difference = list_of_times[list_of_times_index] - list_of_times[list_of_times_index + 1]
+            # log differences between 31 and 33 seconds
+            if td_31 < difference < td_33:
+                logging.warning(f"Difference is: {difference.seconds} seconds"
+                                f"\n{filtered_log[list_of_times_index]}"
+                                f"\n{filtered_log[list_of_times_index + 1]}")
+            # log differences more or equal 33 seconds
+            elif difference >= td_33:
+                logging.error(f"Difference is: {difference.seconds} seconds"
+                              f"\n{filtered_log[list_of_times_index]}"
+                              f"\n{filtered_log[list_of_times_index + 1]}")
+            list_of_times_index += 1
+
+
+file_content = convert_log_to_list_of_strings('hblog.txt')
+filtered_log = filter_log_by_key(file_content, 'Key TSTFEED0300|7E3E|0400')
+analyze_log(filtered_log)
