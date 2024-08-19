8856f29fdd9fe6f67e7cd3045d51ed20f9dc6340
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-08-19 23:47:28 +0300

    lesson 22: add association table

---
+++ lesson_22/tables.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from lesson_22.alchemy_base import Base
+
+association_table = Table(
+    'association_table',
+    Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id'), primary_key=True),
+    Column('course_id', Integer, ForeignKey('courses.course_id', primary_key=True))
+)
+
+
+class CoursesTable(Base):
+    __tablename__ = 'courses'
+
+    course_id = Column(Integer, primary_key=True)
+    course_title = Column(String)
+
+    students = relationship('StudentsTable', secondary=association_table, back_populates='course')
+
+
+class StudentsTable(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    course = relationship('CoursesTable', secondary=association_table, back_populates='students')
