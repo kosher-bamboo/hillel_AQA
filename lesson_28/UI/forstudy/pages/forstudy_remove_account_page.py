7eb216f68e7e396eff05deff03b20bf79b9094dd
kosher-bamboo <kosher-bamboo@tutamail.com>
2024-09-24 15:45:58 +0300

    lesson_31: create docker-compose-jenkins.yaml add driver options update requirements.txt add pipeline-job add allure fixtures to test_search_by_track_number

--- lesson_28/UI/forstudy/pages/forstudy_remove_account_page.py
+++ lesson_28/UI/forstudy/pages/forstudy_remove_account_page.py
@@ -1,5 +1,6 @@
 from lesson_28.UI.base_page import BasePage
 from lesson_28.UI.forstudy.elements.forstudy_remove_account_elements import ForstudyRemoveAccountElements
+import allure
 
 
 class RemoveAccountPage(BasePage):
@@ -7,14 +8,17 @@ class RemoveAccountPage(BasePage):
         super().__init__(driver=driver)
         self.elements = ForstudyRemoveAccountElements()
 
+    @allure.step("Click Delete my Account")
     def click_delete_my_account(self):
         self.click_on_element(locator=self.elements.REMOVE_MY_ACCOUNT_BUTTON, timeout=2)
         return self
 
+    @allure.step("Click Cancel button")
     def click_cancel(self):
         self.click_on_element(locator=self.elements.CANCEL_BUTTON)
         return self
 
+    @allure.step("Click Remove Button")
     def click_remove(self):
         self.click_on_element(locator=self.elements.REMOVE_BUTTON)
         return self
--- lesson_28/ui_tests/test_forstudy.py
+++ lesson_28/ui_tests/test_forstudy.py
@@ -2,6 +2,7 @@ import pytest
 from lesson_28.logger import logger
 from lesson_28.ui_tests.conftest import check_last_name, check_name, remove_account, log_in_user
 from faker import Faker
+import allure
 
 fake = Faker()
 
@@ -11,6 +12,10 @@ email = fake.email()
 password = fake.password()
 
 
+@pytest.mark.forstudy
+@allure.epic("UI Tests")
+@allure.feature("Sign up to qauto2.forstudy.space")
+@allure.story("E2E Sign up")
 def test_forstudy_sign_up(driver, open_main_page, register_user, check_user_is_logged_in, log_out_user,
                           log_in_user, remove_account):
     open_main_page()
@@ -28,10 +33,14 @@ def test_forstudy_sign_up(driver, open_main_page, register_user, check_user_is_l
     remove_account()
 
 
+@pytest.mark.forstudy
 @pytest.mark.parametrize("field, expected_value", [
     ("name", name),  # Check name
     ("last_name", last_name)  # Check last name
 ])
+@allure.epic("UI Tests")
+@allure.feature("Sign up to qauto2.forstudy.space")
+@allure.story("Check {field}")
 def test_forstudy_check_name(driver, open_main_page, register_user, log_in_user, check_name, check_last_name,
                              field, expected_value, remove_account):
     if field == "name":
